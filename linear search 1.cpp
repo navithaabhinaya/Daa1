b4ccebb332b6f0c65d9be609177f2edefb747e34
navithaabhinaya <139116142+navithaabhinaya@users.noreply.github.com>
2023-07-12 09:16:22 +0530

    Add files via upload

---
+++ linear search 1.cpp
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int n, arr[100] , i, count ;
+    cout<<"Enter the number of elements to be sorted \n";
+    cin>>count;
+ 
+    cout<<"Enter " << count <<" numbers\n";
+    
+    for(i=0; i<count; i++)
+    {
+        cin>>arr[i];
+        
+    }
+    
+    cout<<"\n Enter the element to be searched :\n";
+    cin>>n;
+    for(i=0; i<count; i++)
+    {
+        if(arr[i]==n)
+        {
+            cout<<"Element found at position "<<i;
+            break;
+        }
+    }
+    if(i==count)
+    {
+        cout<<"\nElement not found\n";
+    }
+    return 0;
+}
